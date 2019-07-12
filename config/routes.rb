ad0d23ae642dfe6e139fd1cc2e7c4e3fbc8e620e
Vivek Tripathi <tvivek@apexft.com>
2019-07-12 08:25:47 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :notes
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'notes#index'
+end
